6236b1987f7c3a04ac4ee6102e962bac413c792e
Yogesh Khati <yk23888@gmail.com>
2018-07-11 23:41:07 -0700

    Update deploy.rb

--- java/recipes/deploy.rb
+++ java/recipes/deploy.rb
@@ -1,9 +1,7 @@
-node[:deploy].each do |application, deploy|
 
     execute "run_jar" do
-        user                "#{deploy[:user]}"
-        cwd                 "#{deploy[:deploy_to]}/current"
+        user                "deploy"
+        cwd                 "srv/www/app-gigyacurrent"
         command             "nohup /usr/bin/java8 -jar #{node[application.to_s]['jar']} > /var/log/awslogs.log 2> /var/log/awslogs.log < /dev/null &"
     end
 
-end
