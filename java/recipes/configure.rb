90f629408bee65670faf71658220e280fdc678db
Yogesh Khati <yk23888@gmail.com>
2018-07-11 17:36:45 -0700

    add logging

--- java/recipes/configure.rb
+++ java/recipes/configure.rb
@@ -28,7 +28,7 @@ node[:deploy].each do |application, deploy|
     execute "run_jar" do
         user                "#{deploy[:user]}"
         cwd                 "#{deploy[:deploy_to]}/current"
-        command             "nohup /usr/bin/java8 -jar #{node[application.to_s]['jar']} > log.txt 2> error.txt < /dev/null &"
+        command             "nohup /usr/bin/java8 -jar #{node[application.to_s]['jar']} > /var/log/awslogs.log 2> /var/log/awslogs.log < /dev/null &"
     end
 
 end
