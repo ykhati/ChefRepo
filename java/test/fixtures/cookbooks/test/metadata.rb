da19485e27e3a52a46c150a2f15a044b7eb8294f
Yogesh Khati <yk23888@gmail.com>
2018-07-11 17:08:19 -0700

    remove java

--- java/test/fixtures/cookbooks/test/metadata.rb
+++
@@ -1,6 +0,0 @@
-name             'test'
-maintainer       'test cookbook'
-license          'Apache-2.0'
-description      'A test cookbook for the java cookbook'
-version          '0.1.0'
-depends          'java'
--- java/test/fixtures/cookbooks/test/recipes/openjdk7.rb
+++
@@ -1,5 +0,0 @@
-node.default['java']['jdk_version'] = '7'
-
-include_recipe 'test_java::base'
-include_recipe 'java::default'
-include_recipe 'test_java::java_cert'
