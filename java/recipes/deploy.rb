221bb286a041365f49d35c5827be00dfea0164ab
Yogesh Khati <yk23888@gmail.com>
2018-07-11 18:11:15 -0700

    Update deploy.rb

--- java/recipes/deploy.rb
+++ java/recipes/deploy.rb
@@ -8,23 +8,6 @@ node[:deploy].each do |application, deploy|
         next
     end
 
-    template "#{deploy[:deploy_to]}/current/parameters.yml" do
-        source              "parameters.yml.erb"
-        mode                0644
-        group               "root"
-        owner               "#{deploy[:user]}"
-        variables(
-            :parameters =>  node[:deploy][application.to_s][:environment_variables],
-            :application => application
-        )
-    end
-
-    execute "stop_jar" do
-        user                "#{deploy[:user]}"
-        command             "pkill -cf #{node[application.to_s]['jar']}"
-        only_if             "pgrep -f #{node[application.to_s]['jar']}"
-    end
-
     execute "run_jar" do
         user                "#{deploy[:user]}"
         cwd                 "#{deploy[:deploy_to]}/current"
